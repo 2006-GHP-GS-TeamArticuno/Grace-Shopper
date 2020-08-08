909f5652a75d996f1c82c563a749ee63f50224d8
Ariel Ng <ng.ariel.elizabeth@gmail.com>
2020-08-08 19:21:40 -0400

    order confirmation page update

--- client/components/CheckoutConfirmation.js
+++ client/components/CheckoutConfirmation.js
@@ -5,8 +5,18 @@ class CheckoutConfirmation extends React.Component {
   render() {
     return (
       <div align="center">
-        <div>Thank you for your order!</div>
-        <div>Your order is #</div>
+        <br />
+        <h1 className="title is-5" id="singleTitle">
+          {' '}
+          Thank you for your Order, it's almost...
+          <br />
+          <img
+            id="welcomeIMG"
+            src="https://media1.giphy.com/media/f9RHPRFxJhKTF0MQth/giphy.gif?cid=ecf05e4797qn3f7690y9pidr7w208pgomccfzoj98sjt29uz&rid=giphy.gif"
+          />
+          <br />
+          PARTY TIME!
+        </h1>
       </div>
     )
   }
