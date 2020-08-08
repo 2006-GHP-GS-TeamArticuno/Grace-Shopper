d933f18bf10250e99e7ce72afc31bb24cfcf911c
Ariel Ng <ng.ariel.elizabeth@gmail.com>
2020-08-08 16:01:29 -0400

    user-home update

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -9,8 +9,15 @@ export const UserHome = props => {
   const {email} = props
 
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
+    <div className="has-text-centered">
+      <br />
+      <h3 className="title" id="singleTitle">
+        Welcome, {email} !
+      </h3>
+      <img
+        id="welcomeIMG"
+        src="https://media3.giphy.com/media/3o6fJ0mUt4WWF1qox2/giphy.gif?cid=ecf05e47iq8ohvy8ghhvlb3e9tvum6rw76172vxpac1rw8dp&rid=giphy.gif"
+      />
     </div>
   )
 }
